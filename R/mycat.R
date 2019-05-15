a542b548b801aa50bb852d0314642d0c7eb2d335
dusty-turner <dusty.s.turner@gmail.com>
2019-05-14 20:13:43 -0400

    Start Cat Function

---
+++ R/mycat.R
@@ -0,0 +1,10 @@
+#' Cat Function
+#'
+#' @return ASCII Cat
+#' @export
+#'
+#' @examples
+#' mycat()
+mycat = function(CatName = NULL) {
+  cat(paste("\n \  /\\_/\\\n \ ( o.o )\n   > ^ <\n  (     )\n   ^   ^ \n   _____"))
+}
