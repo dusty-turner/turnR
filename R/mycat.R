a48a60c34776a77f76ec83834ec3e175de70f39e
dusty-turner <dusty.s.turner@gmail.com>
2019-05-14 20:38:40 -0400

    Update Cat Function

--- R/mycat.R
+++ R/mycat.R
@@ -1,10 +1,15 @@
 #' Cat Function
 #'
-#' @return ASCII Cat
+#' @param CatName A Cat Name
+#' @return ASCII Cat with its name.  An incorrect data type will return an angry cat.
 #' @export
 #'
 #' @examples
 #' mycat()
-mycat = function(CatName = NULL) {
-  cat(paste("\n \  /\\_/\\\n \ ( o.o )\n   > ^ <\n  (     )\n   ^   ^ \n   _____"))
+mycat = function(CatName = "Beat Navy") {
+  if (is.character(CatName)==TRUE) {
+    cat(paste("Your Cat's Name is", CatName, "\n \  /\\_/\\\n \ ( o.o )\n   > ^ <\n  (     )\n   ^   ^ \n   _____"))
+  } else {
+    cat(paste("That's not a real name", "\n Here is an angry cat", "\n/\\ _^_ /\\"))
+  }
 }
